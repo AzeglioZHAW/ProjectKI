f8d8d21ed92cd030c9d358c95ffc70856fb65179
martiaze <martiaze@zhaw.ch>
2023-06-19 20:51:02 +0200

    Frontend

---
+++ Frontend/src/routes.js
@@ -0,0 +1,16 @@
+// Pages
+
+import StreetsignsClassification from "./pages/Streetsigns.svelte";
+
+
+
+export default {
+    // Home
+    '/': StreetsignsClassification,
+  
+    //cats (leopard, tiger, lion, cheeta)
+    '/streetsigns': StreetsignsClassification,
+
+
+
+}
